6315145501bdd90d2f4c20c3bdaa21f9039733b2
Ryan Taheri <sailantht@gmail.com>
2021-04-30 17:07:38 +0430

    4.Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,63 @@
 import React from "react";
-import manager from '../img/manager.webp'
+import home1 from '../img/home1.png'
+//Styled
+import styled from 'styled-components'
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>the <span>world</span> a better place</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>to live.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact me for anything.I am professional in everything, you will see </p>
                 <button>Contact me</button>
-            </div>
-            <div className="image">
-                <img src={manager} alt="manager's photo"/>
-            </div>
-        </div>
-    )
-}
+            </Description>
+            <Image>
+                <img src={home1} alt="manager's photo"/>
+            </Image>
+        </About>
+    );
+};
+
+//Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`
+const Description = styled.div`
+  flex: 1;
+  padding-right: 1rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`
+const Hide = styled.div`
+  overflow: hidden;
+`
+
+
 export default AboutSection;

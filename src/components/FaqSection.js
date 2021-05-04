9a576e56135f61a0e49bd3dcfa62f1f83f275728
Ryan Taheri <sailantht@gmail.com>
2021-05-04 17:48:54 +0430

    19.Faq Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,55 +3,40 @@ import Toggle from "./Toggle";
 //Styles
 import styled from "styled-components";
 import {About} from "../styles";
+//Animation
+import {AnimateSharedLayout} from "framer-motion";
 
 
 const FaqSection = () => {
     return (
         <Faq className="faq">
             <h2>Any Question <span>FAQ</span></h2>
-            <Toggle>
-                <div className="question">
-                    <h4>How Do I Start?</h4>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
                     <div className="answer">
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
                     </div>
-                    <div className="faq-line">
-
+                </Toggle>
+                <Toggle title="Daily Schedule">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
                     </div>
-                </div>
-            </Toggle>
-
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
-                </div>
-                <div className="faq-line">
-
-                </div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
-                </div>
-                <div className="faq-line">
-
-                </div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
-                </div>
-                <div className="faq-line">
-
-                </div>
-            </div>
+                </Toggle>
+                <Toggle title="Different Payment Methods">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
+                    </div>
+                </Toggle>
+                <Toggle title="What products do you offer?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, unde.</p>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     )
 
